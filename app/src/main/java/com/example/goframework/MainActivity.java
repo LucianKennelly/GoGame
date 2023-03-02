cd15e8e516fb51ef960babe1a8ebab412ea27109
sisourat25 <sisourat25@up.edu>
2023-03-02 10:43:07 -0800

    Initial commit

---
+++ app/src/main/java/com/example/goframework/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.goframework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.GameFramework.GameMainActivity;
+import com.example.GameFramework.LocalGame;
+import com.example.GameFramework.gameConfiguration.GameConfig;
+import com.example.GameFramework.infoMessage.GameState;
+
+public class MainActivity extends GameMainActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+
+    public GameConfig createDefaultConfig(){
+        return null;
+    }
+
+    public LocalGame createLocalGame(GameState gameState) {
+        return null;
+    }
+}
