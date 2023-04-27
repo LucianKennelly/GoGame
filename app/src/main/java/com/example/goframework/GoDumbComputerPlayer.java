e9de4c382d143dba9b0117132b1960f1886d5033
Colin Miller <colin.d.miller@icloud.com>
2023-04-27 10:28:40 -0700

    added comments

--- app/src/main/java/com/example/goframework/GoDumbComputerPlayer.java
+++ app/src/main/java/com/example/goframework/GoDumbComputerPlayer.java
@@ -15,9 +15,11 @@ public class GoDumbComputerPlayer extends GameComputerPlayer {
         super(name);
     }
 
+    // method to receive updates from the game
     @Override
     protected void receiveInfo(GameInfo info) {
 
+        // if the info is not a GoGameState, return
         if (!(info instanceof GoGameState)) {
             return;
         }
@@ -25,22 +27,25 @@ public class GoDumbComputerPlayer extends GameComputerPlayer {
 
             GoGameState copyState = (GoGameState) info;
 
+            // if the game is over for player 1, skip turn
             if(copyState.getGameContinueOne() == false) {
                 GoSkipTurnAction toSend = new GoSkipTurnAction(this);
                 game.sendAction(toSend);
                 return;
             }
+            // otherwise, make a random move
             else {
-
                 int xCoor = (int) (copyState.boardSize * Math.random());
                 int yCoor = (int) (copyState.boardSize * Math.random());
 
+                // keep generating random moves until one is found that is legal
                 while (copyState.getGameBoard(xCoor, yCoor) != EMPTY) {
                     xCoor = (int) (copyState.boardSize * Math.random());
                     yCoor = (int) (copyState.boardSize * Math.random());
                 }
 
-                //sleep(0.001);
+                // sleep(0.001);
+                // create and send a GoPlacePieceAction for the random move
                 GoPlacePieceAction toSend = new GoPlacePieceAction(this, xCoor, yCoor);
                 game.sendAction(toSend);
             }
